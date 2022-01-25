e9d588fe89aed2a634095c760d15cca5ee8aaee4
Daniel <cdeh88@gmail.com>
2022-01-25 10:12:48 -0400

    fix route

--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -18,7 +18,6 @@ export class MemberListComponent implements OnInit {
 
   loadMembers() {
     this.memberService.getMembers().subscribe((members) => {
-      console.log(members)
       this.members = members;
     });
   }
