eebd752ae5ed2290867fb92e5ef462d8973f733e
Daniel <cdeh88@gmail.com>
2022-01-24 21:54:03 -0400

    add member list style and animation

---
+++ API/Data/Migrations/20220125010758_RenameURlToUrl.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Data.Migrations
+{
+    public partial class RenameURlToUrl : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "URl",
+                table: "Photos",
+                newName: "Url");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Url",
+                table: "Photos",
+                newName: "URl");
+        }
+    }
+}
