3717c0b1799cf4a454e4f4ee9391a7d09fe9f595
Daniel <cdeh88@gmail.com>
2022-08-16 09:18:46 -0400

    refactor dataContext with Identity

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -1,21 +1,36 @@
 using API.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Data
 {
-    public class DataContext : DbContext
+    public class DataContext : IdentityDbContext<AppUser, AppRole, int, 
+        IdentityUserClaim<int>, AppUserRole, IdentityUserLogin<int>, 
+        IdentityRoleClaim<int>, IdentityUserToken<int>>
     {
         public DataContext(DbContextOptions options) : base(options)
         {
         }
 
-        public DbSet<AppUser> Users { get; set; }
         public DbSet<UserLike> Likes { get; set; }
         public DbSet<Message> Messages { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
+
+            builder.Entity<AppUser>()
+                .HasMany(usr => usr.UserRoles)
+                .WithOne(u => u.User)
+                .HasForeignKey(usr => usr.UserId)
+                .IsRequired();
+
+            builder.Entity<AppRole>()
+                .HasMany(usr => usr.UserRoles)
+                .WithOne(u => u.Role)
+                .HasForeignKey(usr => usr.RoleId)
+                .IsRequired();
 
             builder.Entity<UserLike>().HasKey(k => new { k.SourceUserId, k.LikedUserId });
 
@@ -39,7 +54,7 @@ namespace API.Data
             builder.Entity<Message>()
                 .HasOne(u => u.Sender)
                 .WithMany(m => m.MessagesSent)
-                .OnDelete(DeleteBehavior.Restrict); 
+                .OnDelete(DeleteBehavior.Restrict);
         }
     }
 }
